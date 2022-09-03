878ab70fa48ecb66d54ed0d329b774fe021b6b46
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-09-02 23:58:48 -0500

    StringsToDo1 assignment - start

---
+++ StringsToDo1.js
@@ -0,0 +1,24 @@
+// Remove Blanks
+function removeBlanks1() {
+  let text = "Pl ayTha tF u nkyM usi c";
+  let result = text.replace(/\s/g, "");
+  console.log(result);
+}
+removeBlanks1();
+// Expected result: PlayThatFunkyMusic
+
+function removeBlanks2() {
+  let text = "I can not BELIEVE it's not BUTTER";
+  let result = text.replace(/\s/g, "");
+  console.log(result);
+}
+removeBlanks2();
+// Expected result: IcannotBELIEVEit'snotBUTTER
+
+// Get Digits
+
+//  Acronyms
+
+// Court Non-Spaces
+
+// Remove Shorter Strings
