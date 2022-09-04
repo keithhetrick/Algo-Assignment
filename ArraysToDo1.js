c0a3d3463a8ed6a09c860507a4462009540f21c5
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-09-04 00:06:46 -0500

    ArraysToDo1 - update

--- ArraysToDo1.js
+++ ArraysToDo1.js
@@ -1,74 +1,106 @@
 // Push Front
-let arr1 = [5, 7, 2, 3];
-function pushFront1() {
-  arr1.unshift(8);
-  console.log(arr1);
+function pushToFrontOne() {
+  let arr = [5, 7, 2, 3];
+  let val = 8;
+  arr.push(0);
+  for (let i = arr.length - 1; i > 0; i--) {
+    arr[i] = arr[i - 1];
+  }
+  arr[0] = val;
+  console.log("pushToFrontOne :", arr);
 }
-pushFront1();
+pushToFrontOne();
 // Expected result: [ 8, 5, 7, 2, 3 ]
 
-let arr2 = [99];
-function pushFront2() {
-  arr2.unshift(7);
-  console.log(arr2);
+function pushToFrontTwo() {
+  let arr = [99];
+  let val = 7;
+  arr.push(0);
+  for (let i = arr.length - 1; i > 0; i--) {
+    arr[i] = arr[i - 1];
+  }
+  arr[0] = val;
+  console.log("pushToFrontTwo : ", arr);
 }
-pushFront2();
+pushToFrontTwo();
+// Expected result:  [ 7, 99 ]
+
+function pushToFrontOneAlt() {
+  let arr = [5, 7, 2, 3];
+  arr.unshift(8);
+  console.log("pushToFrontOneAlt : ", arr);
+}
+pushToFrontOneAlt();
+// Expected result: [ 8, 5, 7, 2, 3 ]
+
+function pushToFrontTwoAlt() {
+  let arr = [99];
+  arr.unshift(7);
+  console.log("pushToFrontTwoAlt : ", arr);
+}
+pushToFrontTwoAlt();
 // Expected result: [ 7, 99 ]
 
 // Pop Front
-let arr3 = [0, 5, 10, 15];
-function popFront1() {
-  arr3.shift();
-  console.log(arr3);
+function popFrontOne() {
+  let arr = [0, 5, 10, 15];
+  arr.shift();
+  console.log("popFrontOne : ", arr);
 }
-popFront1();
+popFrontOne();
 // Expected result: 5, 10, 15
 
-let arr4 = [4, 5, 7, 9];
-function popFront2() {
-  arr4.shift();
-  console.log(arr4);
+function popFrontTwo() {
+  let arr = [4, 5, 7, 9];
+  arr.shift();
+  console.log("popFrontTwo : ", arr);
 }
-popFront2();
+popFrontTwo();
 // Expected result: [5, 7, 9]
 
 // Insert At - Variations at insertAt3 & insertAt4
-function insertAt1() {
-  let arr5 = [100, 200, 5];
-  let newArr1 = arr5.splice(2, 0, 311);
-  return arr5;
+function insertAtOne() {
+  arr = [100, 200, 5];
+  index = 2;
+  value = 311;
+  for (let i = arr.length; i >= index; i--) {
+    arr[i] = arr[i - 1];
+  }
+  arr[index] = value;
+  return arr;
 }
-insertAt1();
-console.log(insertAt1());
+insertAtOne();
+console.log("insertAtOne : ", insertAtOne());
 // Expected result: [ 100, 200, 311, 5 ]
 
-function insertAt2() {
-  let arr6 = [9, 33, 7];
-  let newArr2 = arr6.splice(1, 0, 42);
-  return arr6;
-}
-insertAt2();
-console.log(insertAt2());
-// Expected result: [ 9, 42, 33, 7  ]
-
-function insertAt3(arr7, index, value) {
-  for (let i = arr7.length; i >= index; i--) {
-    arr7[i] = arr7[i - 1];
+function insertAtTwo() {
+  arr = [9, 33, 7];
+  index = 1;
+  value = 42;
+  for (let i = arr.length; i >= index; i--) {
+    arr[i] = arr[i - 1];
   }
-  arr7[index] = value;
-  return arr7;
+  arr[index] = value;
+  return arr;
 }
-insertAt3([100, 200, 5], 2, 311);
-console.log(insertAt3([100, 200, 5], 2, 311));
-// Expected result: [ 100, 200, 311, 5 ]
-
-function insertAt4(arr8, index, value) {
-  for (let i = arr8.length; i >= index; i--) {
-    arr8[i] = arr8[i - 1];
-  }
-  arr8[index] = value;
-  return arr8;
-}
-insertAt4([9, 33, 7], 1, 42);
-console.log(insertAt4([9, 33, 7], 1, 42));
+insertAtTwo();
+console.log("insertAtTwo : ", insertAtTwo());
 // Expected result: [ 9, 42, 33, 7 ]
+
+function insertAtOneAlt() {
+  let arr = [100, 200, 5];
+  let newArr1 = arr.splice(2, 0, 311);
+  return arr;
+}
+insertAtOneAlt();
+console.log("insertAtOneAlt : ", insertAtOneAlt());
+// Expected result: [ 100, 200, 311, 5 ]
+
+function insertAtTwoAlt() {
+  let arr = [9, 33, 7];
+  let newArr2 = arr.splice(1, 0, 42);
+  return arr;
+}
+insertAtTwoAlt();
+console.log("insertAtTwoAlt : ", insertAtTwoAlt());
+// Expected result: [ 9, 42, 33, 7  ]
